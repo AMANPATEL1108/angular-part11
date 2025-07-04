8e9a416b8b9ab479da6420c278591f549ae021fb
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-07-04 08:47:50 +0530

    Simple Api Called

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Products } from './services/products';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  producData: any;
   protected title = 'angular-part11';
 
   constructor(private productService: Products) {}
   ngOnInit() {
-    this.productService.getProductList().subscribe((data: any) => { 
-      
+    this.productService.getProductList().subscribe((data: any) => {
+      this.producData = data.products;
     });
   }
 }
